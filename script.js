f09c4d5782d2b20f840a0c9a9cabe194b1843b14
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-22 09:37:18 +0000

    edit function called createUserCard

--- script.js
+++ script.js
@@ -15,6 +15,27 @@ async function getUser(username) {
 
 function createUserCard(user) {
   const cardHTML = `
+<div class="card">
+<div>
+<img class="avatar" src="https://randomuser.me/api/portraits/women/30.jpg" alt="">
+</div>
+<div class="user-info">
+<h2>Emily Green</h2>
+<p>Adipisicing consequatur tempora expedita suscipit elit perferendis. Quasi facilis laborum</p>
+
+<ul>
+  <li>300 <strong>Followers</strong></li>
+  <li>100 <strong>Following</strong></li>
+  <li>30 <strong>Repos</strong></li>
+</ul>
+
+<div class="repos">
+<a class="repo" href="#">Repo 1</a>
+<a class="repo" href="#">Repo 2</a>
+<a class="repo" href="#">Repo 3</a>
+</div>
+</div>
+</div>
 
 
   `;
