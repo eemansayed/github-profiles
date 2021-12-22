07711ba4891af74da6e13f86eae5629486d2ce5f
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-22 10:03:42 +0000

    add function called getRepos

--- script.js
+++ script.js
@@ -9,10 +9,21 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username);
 
     createUserCard(data);
+    getRepos(username);
   } catch (err) {
     if (err.response.status == 404) {
       createErrorCard("No profile with this username");
     }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos");
+
+    addReposToCard(data);
+  } catch (err) {
+    createErrorCard("Problem fetching repos");
   }
 }
 
