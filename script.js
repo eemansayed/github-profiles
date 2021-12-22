fef53be9e9b53f319c4414c68719ba44e3e6b26b
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-22 09:26:34 +0000

    add const called form

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const APIURL = "https://api.github.com/users/";
 
+const form = document.getElementById("form");
+
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
